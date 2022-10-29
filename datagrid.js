429c1eb8179b0fa56245c6a596568ee685a1244f
zackgoodson <zackgoodson@gmail.com>
2022-10-29 11:41:46 -0700

    Website Update

--- datagrid.js
+++ datagrid.js
@@ -1,6 +1,7 @@
 // URL to your production code when you publish yoru google app script as a web app
 // You will get this URL when you publish your google apps script as a web app
-let prefix="YourWebAppURLHere" // looks like: https://script.google.com/macros/s/AKfycbz90JgtbAjrjz8c8WQGjgy8kM2GtdCAulom8XWOQyGoeX8DXM5XsmUMAcSYSSyZwwNSLq/exec
+let prefix="https://script.google.com/macros/s/AKfycbzpkB-fVUMQLcEkBM4WQ3LBXHEUuxypeCYL5eoo_JlRFSKX-OUim-Iqcv9I4_eUZnDk5g/exec" 
+    // looks like: https://script.google.com/macros/s/AKfycbz90JgtbAjrjz8c8WQGjgy8kM2GtdCAulom8XWOQyGoeX8DXM5XsmUMAcSYSSyZwwNSLq/exec
 const dealy_seconds = 4 // how long data validation messages are visible
 
 //example call: file:///C:/Users/Gove/three-tier/datagrid.html?employee
